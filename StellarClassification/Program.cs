5bc4f179ab6f351bbee68cfbb8e204ec3b2fd1a3
Bruno Borges Luza <bluzaborges@hotmail.com>
2022-10-18 00:21:21 -0300

    Refactored code and created redshift index.

--- StellarClassification/Program.cs
+++ StellarClassification/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace StellarClassification;
 
-public struct Dados
+public struct Data
 {
     public int Id { get; set; }
     public string Alpha { get; set; }
@@ -20,7 +20,12 @@ public struct Dados
     public string Plate { get; set; }
     public string MJD { get; set; }
     public string Fiber_id { get; set; }
+}
 
+public struct Index
+{
+    public int Address;
+    public decimal Value;
 }
 
 public static class Program
@@ -29,9 +34,10 @@ public static class Program
     {
         int option = 0;
 
-        int TamanhoRegistro = SaveAsBinary();
+        int DataRegisterSize = SaveAsBinary();
+        int RedshiftIndexRegisterSize = RedshiftIndex(DataRegisterSize);
 
-        while (option != 3)
+        while (option != 5)
         {
             Menu();
             Console.Write("Digite a opção: ");
@@ -43,10 +49,95 @@ public static class Program
                     ShowData();
                     break;
                 case 2:
-                    SearchForId(TamanhoRegistro);
+                    SearchForId(DataRegisterSize);
+                    break;
+                case 3:
+                    ShowRedshiftIndex();
+                    break;
+                case 4:
+                    SearchForRedshiftValue(RedshiftIndexRegisterSize, DataRegisterSize);
                     break;
             }
         }
+    }
+
+    private static int SaveAsBinary()
+    {
+
+        Console.WriteLine("Convertendo dataset...");
+
+        using var reader = new StreamReader("files\\star_classification.csv");
+        using var binaryStream = File.Create("files\\star_classification.dat");
+        using var writer = new BinaryWriter(binaryStream);
+
+        var header = reader.ReadLine();
+        int registerSize = 0;
+
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            var values = line.Split(',').Skip(0).ToArray();
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = values[i].PadLeft(20);
+            }
+
+            writer.Write(string.Join(',', values));
+
+            registerSize = System.Text.ASCIIEncoding.ASCII.GetByteCount(string.Join(',', values));
+        }
+
+        Console.WriteLine("Arquivo binário do dataset criado com sucesso.");
+        Console.Clear();
+
+        return registerSize + 2;
+    }
+
+    private static int RedshiftIndex(int DataRegistrySize)
+    {
+
+        Console.WriteLine("Criando índice do campo redshift...");
+
+        using var dataBinaryStream = File.Open("files\\star_classification.dat", FileMode.Open);
+        using var dataReader = new BinaryReader(dataBinaryStream);
+
+        var header = dataReader.ReadString();
+
+        long numberOfRegisters = dataReader.BaseStream.Length / DataRegistrySize;
+
+        Index[] redShiftIndex = new Index[numberOfRegisters];
+
+        int i = 0;
+        while (dataReader.BaseStream.Position != dataReader.BaseStream.Length)
+        {
+            var line = dataReader.ReadString();
+            var values = line.Split(',').Skip(0).ToArray();
+
+            redShiftIndex[i].Address = int.Parse(values[0]);
+            redShiftIndex[i].Value = Decimal.Parse(values[14], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture);
+
+            i++;
+        }
+
+        MergeSort(redShiftIndex, 0, (int)numberOfRegisters - 1);
+
+        using var RedshiftIndex = File.Create("files\\red_shift_index.dat");
+        using var indexWriter = new BinaryWriter(RedshiftIndex);
+
+        int sizeOfRegister = 0;
+
+        for (i = 0; i < numberOfRegisters - 1; i++)
+        {
+            string register = $"{redShiftIndex[i].Address.ToString().PadLeft(5)};{redShiftIndex[i].Value.ToString().PadLeft(20)}";
+            indexWriter.Write(register);
+            sizeOfRegister = System.Text.ASCIIEncoding.ASCII.GetByteCount(register);
+        }
+
+        Console.WriteLine("Índice de redshift criado com sucesso.");
+        Console.Clear();
+
+        return sizeOfRegister + 1;
     }
 
     private static void AssembleFile()
@@ -75,43 +166,42 @@ public static class Program
 
         var header = reader.ReadString();
 
-        Dados dados = new Dados();
+        Data data = new Data();
 
-        Console.WriteLine("   ID    ALPHA              DELTA                   REDSHIFT                 CLASSE");
+        Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+        Console.WriteLine("   ID            CLASSE         REDSHIFT                    MJD      ALPHA             DELTA");
 
         while (reader.BaseStream.Position != reader.BaseStream.Length)
         {
             var line = reader.ReadString();
             var values = line.Split(',').Skip(0).ToArray();
 
-            dados.Id = int.Parse(values[0]);
-            dados.Alpha = values[1];
-            dados.Delta = values[2];
-            dados.U = values[3];
-            dados.G = values[4];
-            dados.R = values[5];
-            dados.I = values[6];
-            dados.Z = values[7];
-            dados.Run_id = values[8];
-            dados.Rerun_id = values[9];
-            dados.Cam_col = values[10];
-            dados.Field_id = values[11];
-            dados.Spec_obj_id = values[12];
-            dados.Class = values[13];
-            dados.Redshift = values[14];
-            dados.Plate = values[15];
-            dados.MJD = values[16];
-            dados.Fiber_id = values[17];
+            data.Id = int.Parse(values[0]);
+            data.Alpha = values[1];
+            data.Delta = values[2];
+            //data.U = values[3];
+            //data.G = values[4];
+            //data.R = values[5];
+            //data.I = values[6];
+            //data.Z = values[7];
+            //data.Run_id = values[8];
+            //data.Rerun_id = values[9];
+            //data.Cam_col = values[10];
+            //data.Field_id = values[11];
+            //data.Spec_obj_id = values[12];
+            data.Class = values[13];
+            data.Redshift = values[14];
+            //data.Plate = values[15];
+            data.MJD = values[16];
+            //data.Fiber_id = values[17];
 
-            Console.WriteLine($"{dados.Id.ToString().PadLeft(5)}{dados.Alpha}{dados.Delta}{dados.Redshift}{dados.Class}");
+            Console.WriteLine($"{data.Id.ToString().PadLeft(5)}{data.Class}{data.Redshift}{data.MJD}{data.Alpha}{data.Delta}");
         }
-    }
 
-    private static void Menu()
-    {
-        Console.WriteLine("1. Mostrar todos os dados.");
-        Console.WriteLine("2. Pesquisar por id.");
-        Console.WriteLine("3. Sair.");
+        Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+        Console.WriteLine("Aperte qualquer tecla para continuar");
+        Console.ReadLine();
+        Console.Clear();
     }
 
     private static void SearchForId(int TamanhoRegistro)
@@ -119,75 +209,217 @@ public static class Program
         using var binaryStream = File.Open("files\\star_classification.dat", FileMode.Open);
         using var reader = new BinaryReader(binaryStream);
 
-        Console.Write("Digite o id: ");
+        Console.Write("Digite o ID: ");
         int Id = int.Parse(Console.ReadLine());
 
         var header = reader.ReadString();
 
-        if (BinarySearch(reader, Id, 0, reader.BaseStream.Length / TamanhoRegistro - 1, TamanhoRegistro) == -1)
+        if (BinarySearchForId(reader, Id, 0, reader.BaseStream.Length / TamanhoRegistro - 1, TamanhoRegistro) == -1)
         {
-            Console.WriteLine("Id nao encontrado");
+            Console.WriteLine("ID não encontrado.");
         }
+
+        Console.WriteLine("Aperte qualquer tecla para continuar");
+        Console.ReadLine();
+        Console.Clear();
     }
 
-    private static int SaveAsBinary()
+    private static void ShowRedshiftIndex()
     {
-        using var reader = new StreamReader("files\\star_classification.csv");
-        using var binaryStream = File.Create("files\\star_classification.dat");
-        using var writer = new BinaryWriter(binaryStream);
+        using var binaryStream = File.Open("files\\red_shift_index.dat", FileMode.Open);
+        using var reader = new BinaryReader(binaryStream);
 
-        var header = reader.ReadLine();
-        int TamanhoRegistro = 0;
-        int cont = 0;
+        Console.WriteLine("------------------------------");
 
-        while (!reader.EndOfStream)
+        Console.WriteLine("    ID        REDSHIFT");
+
+        while (reader.BaseStream.Position != reader.BaseStream.Length)
         {
-            var line = reader.ReadLine();
+            var line = reader.ReadString();
+            var values = line.Split(';').Skip(0).ToArray();
+
+            Console.WriteLine($"{values[0].PadLeft(6)}{values[1]}");
+        }
+
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("Aperte qualquer tecla para continuar");
+        Console.ReadLine();
+        Console.Clear();
+    }
+
+    private static void SearchForRedshiftValue(int RedshiftIndexRegisterSize, int DataRegisterSize)
+    {
+        using var indexBinaryStream = File.Open("files\\red_shift_index.dat", FileMode.Open);
+        using var indexReader = new BinaryReader(indexBinaryStream);
+        using var dataBinaryStream = File.Open("files\\star_classification.dat", FileMode.Open);
+        using var dataReader = new BinaryReader(dataBinaryStream);
+
+        Console.Write("Digite um valor de redshit: ");
+        decimal redShift = decimal.Parse(Console.ReadLine());
+
+        int foundId = RedshiftBinarySearch(indexReader, redShift, 0, indexReader.BaseStream.Length / RedshiftIndexRegisterSize - 1, RedshiftIndexRegisterSize);
+
+        if (foundId != -1)
+        {
+            dataReader.BaseStream.Seek(foundId * DataRegisterSize, SeekOrigin.Begin);
+            var line = dataReader.ReadString();
             var values = line.Split(',').Skip(0).ToArray();
-
-            for (int i = 0; i < values.Length; i++)
-            {
-                values[i] = values[i].PadLeft(20);
-            }
-
-            writer.Write(string.Join(',', values));
-
-            TamanhoRegistro = System.Text.ASCIIEncoding.ASCII.GetByteCount(string.Join(',', values));
+            WriteRegister(foundId, values[13], values[14], values[16], values[1], values[2]);
+        } else
+        {
+            Console.WriteLine("Valor nao encontrado");
         }
 
-        Console.WriteLine("Arquivo binario criado com sucesso.");
-
-        return TamanhoRegistro + 2;
+        Console.WriteLine("Aperte qualquer tecla para continuar");
+        Console.ReadLine();
+        Console.Clear();
     }
 
-    private static int BinarySearch(BinaryReader reader, int Id, long Inferior, long Superior, int TamanhoRegistro)
+    private static void Merge(Index[] array, int start, int middle, int end)
+    {
+        int n1 = middle - start + 1;
+        int n2 = end - middle;
+        int i, j;
+        Index[] arrayAux1 = new Index[n1];
+        Index[] arrayAux2 = new Index[n2];
+
+        for (i = 0; i < n1; ++i)
+        {
+            arrayAux1[i].Address = array[start + i].Address;
+            arrayAux1[i].Value = array[start + i].Value;
+        }
+
+        for (j = 0; j < n2; ++j)
+        {
+            arrayAux2[j].Address = array[middle + 1 + j].Address;
+            arrayAux2[j].Value = array[middle + 1 + j].Value;
+        }
+
+        i = 0;
+        j = 0;
+        int k = start;
+
+        while(i < n1 && j < n2)
+        {
+            if (arrayAux1[i].Value <= arrayAux2[j].Value)
+            {
+                array[k].Value = arrayAux1[i].Value;
+                array[k].Address = arrayAux1[i].Address;
+                i++;
+            } else
+            {
+                array[k].Value = arrayAux2[j].Value;
+                array[k].Address = arrayAux2[j].Address;
+                j++;
+            }
+            k++;
+        }
+
+        while(i < n1)
+        {
+            array[k].Value = arrayAux1[i].Value;
+            array[k].Address = arrayAux1[i].Address;
+            i++;
+            k++;
+        }
+
+        while(j < n2)
+        {
+            array[k].Value = arrayAux2[j].Value;
+            array[k].Address = arrayAux2[j].Address;
+            j++;
+            k++;
+        }
+        
+    }
+
+    private static void MergeSort(Index[] array, int start, int end)
+    {
+        if (start < end)
+        {
+            int middle = start + (end - start) / 2;
+
+            MergeSort(array, start, middle);
+            MergeSort(array, middle + 1, end);
+            Merge(array, start, middle, end);
+        }
+    }
+
+    private static int BinarySearchForId(BinaryReader reader, int id, long inferior, long superior, int registerSize)
     {
 
-        if (Superior >= Inferior)
+        if (superior >= inferior)
         {
-            long metade = Inferior + (Superior - Inferior) / 2;
+            long metade = inferior + (superior - inferior) / 2;
 
-            reader.BaseStream.Seek(metade*TamanhoRegistro, SeekOrigin.Begin);
+            reader.BaseStream.Seek(metade * registerSize, SeekOrigin.Begin);
 
             var line = reader.ReadString();
             var values = line.Split(',').Skip(0).ToArray();
             int foundId = int.Parse(values[0]);
 
-            if (foundId == Id)
+            if (foundId == id)
             {
-                Console.WriteLine("   ID    ALPHA              DELTA                   REDSHIFT                 CLASSE");
-                Console.WriteLine($"{foundId.ToString().PadLeft(5)}{values[1]}{values[2]}{values[14]}{values[13]}");
+                WriteRegister(foundId, values[13], values[14], values[16], values[1], values[2]);
                 return 1;
             }
 
-            if (foundId > Id)
+            if (foundId > id)
             {
-                return BinarySearch(reader, Id, Inferior, metade - 1, TamanhoRegistro);
+                return BinarySearchForId(reader, id, inferior, metade - 1, registerSize);
             }
 
-            return BinarySearch(reader, Id, metade + 1, Superior, TamanhoRegistro);
+            return BinarySearchForId(reader, id, metade + 1, superior, registerSize);
         }
 
         return -1;
+    }
+
+    private static int RedshiftBinarySearch(BinaryReader reader, decimal redShift, long inferior, long superior, int registerSize)
+    {
+
+        if (superior >= inferior)
+        {
+            long metade = inferior + (superior - inferior) / 2;
+
+            reader.BaseStream.Seek(metade * registerSize, SeekOrigin.Begin);
+
+            var line = reader.ReadString();
+            var values = line.Split(';').Skip(0).ToArray();
+            decimal foundRedShift = decimal.Parse(values[1]);
+
+            if (foundRedShift == redShift)
+            {
+                return int.Parse(values[0]);
+            }
+
+            if (foundRedShift > redShift)
+            {
+                return RedshiftBinarySearch(reader, redShift, inferior, metade - 1, registerSize);
+            }
+
+            return RedshiftBinarySearch(reader, redShift, metade + 1, superior, registerSize);
+        }
+
+        return -1;
+    }
+
+    private static void WriteRegister(int id, string type, string redshift, string mjd, string alpha, string delta)
+    {
+        Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+        Console.WriteLine("   ID            CLASSE         REDSHIFT                    MJD      ALPHA             DELTA");
+        Console.WriteLine($"{id.ToString().PadLeft(5)}{type}{redshift}{mjd}{alpha}{delta}");
+        Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+    }
+
+    private static void Menu()
+    {
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine("1. Mostrar todos os dados.");
+        Console.WriteLine("2. Pesquisar por id.");
+        Console.WriteLine("3. Mostrar indice de redshift.");
+        Console.WriteLine("4. Pesquisar por valor de redshift.");
+        Console.WriteLine("5. Sair.");
+        Console.WriteLine("-------------------------------------------");
     }
 }
